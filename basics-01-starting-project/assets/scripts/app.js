e42577251a48f069b3e0f8867a0e8ff4e2b788b5
yhBai <byh1248218020@163.com>
2021-11-02 15:33:35 +0800

    34. Executing Functions "Indirectly"
    There are currently two problems with unconventional calculator:
    1. The input is a character string, after the + operator is performed, the output of "0input" is displayed.
    2. Enter again, the string will still be spliced after the last output.

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,8 +1,13 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-currentResult = (currentResult + 10) * 3 / 2 - 1;
+function add(){
+    currentResult =  currentResult + userInput.value;outputResult(currentResult, '');
+}
 
-let calculationDescription = '(' + defaultResult + ' + 10) * 3 / 2 - 1';
+addBtn.addEventListener('click',add);
 
-outputResult(currentResult, calculationDescription);
+
+
+
+
