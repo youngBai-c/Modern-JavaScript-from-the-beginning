db0d8a3012c292659da70f59a4967b561a81e58a
yhBai <byh1248218020@163.com>
2021-11-02 12:05:01 +0800

    21.Variables & Operators
    First display output on HTML page through constants and variables
    1. Define result variables and result expressions.
    2. Bind to the document element.
    3. HTML get element output and display it.

--- basics-01-starting-project/assets/scripts/vendor.js
+++ basics-01-starting-project/assets/scripts/vendor.js
@@ -4,9 +4,11 @@ const subtractBtn = document.getElementById('btn-subtract');
 const multiplyBtn = document.getElementById('btn-multiply');
 const divideBtn = document.getElementById('btn-divide');
 
+//1.current-result绑定currentResultOutput
 const currentResultOutput = document.getElementById('current-result');
 const currentCalculationOutput = document.getElementById('current-calculation');
 
+//2.Input: 一个变量(结果) & 一个常量(表达式)
 function outputResult(result, text) {
   currentResultOutput.textContent = result;
   currentCalculationOutput.textContent = text;
