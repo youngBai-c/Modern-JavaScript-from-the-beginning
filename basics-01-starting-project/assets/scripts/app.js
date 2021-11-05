041446947f35802b7de3923867a452d164c54f24
yhBai <byh1248218020@163.com>
2021-11-05 21:45:20 +0800

    simple calculator
    This calculator satisfies basic operations (+-* /)

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,13 +1,56 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+// This is  a comment
+/** 
+ * This is
+ * a comment
+ * **/
+
+function getUserInput(){
+    return parseInt(userInput.value);
+}
+
+function createAndWriteOutpt(opeartor,resultBeforeCalc,calcNumber){
+    const calcDescription =  `${resultBeforeCalc} ${opeartor} ${calcNumber}`;
+    outputResult(currentResult, calcDescription);
+}
+
 function add(){
-    currentResult =  currentResult + userInput.value;outputResult(currentResult, '');
+    // 不要写重复代码
+    const enteredNumber = getUserInput();
+    const initialResult = currentResult;
+    currentResult +=  enteredNumber
+    // currentResult =  currentResult + +userInput.value;
+    // outputResult(currentResult, callDescription);
+    createAndWriteOutpt('+',initialResult,enteredNumber)
+}
+
+function subtract(){
+    const enteredNumber = getUserInput();
+    const initialResult = currentResult;
+    currentResult -=  enteredNumber
+    createAndWriteOutpt('-',initialResult,enteredNumber)
+}
+
+function multiply(){
+    const enteredNumber = getUserInput();
+    const initialResult = currentResult;
+    currentResult *= enteredNumber
+    createAndWriteOutpt('*',initialResult,enteredNumber)
+}
+
+function divide(){
+    const enteredNumber = getUserInput();
+    const initialResult = currentResult;
+    currentResult /=  enteredNumber
+    createAndWriteOutpt('/',initialResult,enteredNumber)
 }
 
 addBtn.addEventListener('click',add);
-
-
+subtractBtn.addEventListener('click',subtract);
+multiplyBtn.addEventListener('click',multiply);
+displayBtn.addEventListener('click',divide);
 
 
 
